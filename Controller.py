79a0935396bed5377baba6753079f0beb5259f0d
lossless1024 <lossless1024@gmail.com>
2021-01-01 18:47:43 +0100

    Add host parameter to controller

--- Controller.py
+++ Controller.py
@@ -2,6 +2,14 @@ import zmq
 import sys
 
 socket = zmq.Context.instance().socket(zmq.REQ)
-socket.connect('tcp://127.0.0.1:6969')
-socket.send_string(' '.join(sys.argv[1:]))
+if sys.argv[1].startswith("-h"):
+    if sys.argv[1] == "-h":
+        socket.connect('tcp://' + sys.argv[2] + ':6969')
+        socket.send_string(' '.join(sys.argv[3:]))
+    else:
+        socket.connect('tcp://' + sys.argv[1][2:] + ':6969')
+        socket.send_string(' '.join(sys.argv[2:]))
+else:
+    socket.connect('tcp://127.0.0.1:6969')
+    socket.send_string(' '.join(sys.argv[1:]))
 print(socket.recv_string())
