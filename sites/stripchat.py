b214414dd6ea04578062306d9c8461530f8603e3
lossless1024 <lossless1024@gmail.com>
2022-06-02 23:30:56 +0200

    Fix stripchat

--- sites/stripchat.py
+++ sites/stripchat.py
@@ -9,12 +9,13 @@ class StripChat(Bot):
     aliases = ['xhamsterlive']
 
     def getVideoUrl(self):
-        return "https://b-{server}.stripst.com/hls/{id}/master_{id}.m3u8".format(
-                server=self.info["viewServers"]["flashphoner-hls"],
-                id=self.info["model"]["id"]
-            ) or None
+        return "https://b-{server}.{host}/hls/{id}/master_{id}.m3u8".format(
+                server=self.lastInfo["viewCam"]["viewServers"]["flashphoner-hls"],
+                host=self.lastInfo["config"]["data"]["hlsStreamHost"],
+                id=self.lastInfo["viewCam"]["streamName"]
+            )
 
-    def getInfo(self):
+    def getStatus(self):
         headers = {
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
@@ -28,27 +29,26 @@ class StripChat(Bot):
 
         start = b'window.__PRELOADED_STATE__ = '
         end = b'</script>'
+        
+        if r.content.find(start) == -1:
+            return Bot.Status.OFFLINE
+        
         j = r.content[r.content.find(start) + len(start):]
         j = j[:j.find(end)]
+        
         try:
-            data_json = json.loads(j)
+            self.lastInfo = json.loads(j)
         except:
             self.log('Failed to parse JSON')
             return Bot.Status.UNKNOWN
 
-        self.info = data_json["viewCam"]
-
-        if self.info["model"]["status"] == "public":
+        if self.lastInfo["viewCam"]["model"]["status"] == "public":
             return Bot.Status.PUBLIC
-        if self.info["model"]["status"] == "private":
+        if self.lastInfo["viewCam"]["model"]["status"] == "private":
             return Bot.Status.PRIVATE
-        if self.info["model"]["status"] == "off":
+        if self.lastInfo["viewCam"]["model"]["status"] == "off":
             return Bot.Status.OFFLINE
         return Bot.Status.UNKNOWN
-
-    def getStatus(self):
-        r = requests.get(self.getVideoUrl())
-        return Bot.Status(r.status_code)
 
 
 Bot.loaded_sites.add(StripChat)
