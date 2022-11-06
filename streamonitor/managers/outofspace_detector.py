2d4e575a3cfaf2692061f50599e9f4238c56ac55
lossless1024 <lossless1024@gmail.com>
2022-11-06 02:04:00 +0100

    Some fixes in out of space detector

--- streamonitor/managers/outofspace_detector.py
+++ streamonitor/managers/outofspace_detector.py
@@ -1,24 +1,33 @@
+import os.path
 import shutil
 from time import sleep
 import streamonitor.log as log
-from streamonitor.manager import Manager
+from threading import Thread
 from parameters import DOWNLOADS_DIR, MIN_FREE_DISK_PERCENT
 from streamonitor.clean_exit import CleanExit
 
 
-class OOSDetector(Manager):
+class OOSDetector(Thread):
     under_threshold_message = 'Free space is under threshold. Exiting.'
 
     def __init__(self, streamers):
-        super().__init__(streamers)
+        super().__init__()
+        self.streamers = streamers
         self.daemon = True
         self.logger = log.Logger("out_of_space_detector")
 
     @staticmethod
-    def disk_space_good():
-        usage = shutil.disk_usage(DOWNLOADS_DIR)
+    def free_space():
+        if os.path.exists(DOWNLOADS_DIR):
+            usage = shutil.disk_usage(DOWNLOADS_DIR)
+        else:
+            usage = shutil.disk_usage('.')
         free_percent = usage.free / usage.total * 100
-        return free_percent > MIN_FREE_DISK_PERCENT
+        return free_percent
+
+    @staticmethod
+    def disk_space_good():
+        return OOSDetector.free_space() > MIN_FREE_DISK_PERCENT
 
     def run(self):
         while True:
