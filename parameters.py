4a252a514451e2037fcefdf7ef93c6a7e1c084d5
Kseen715 <korenev_deni@edu.bstu.ru>
2023-05-16 00:37:08 +0300

    Minor changes.

--- parameters.py
+++ parameters.py
@@ -1,11 +1,11 @@
 DOWNLOADS_DIR = 'downloads'
-MIN_FREE_DISK_PERCENT = 1.0  # in %
+MIN_FREE_DISK_PERCENT = 30.0  # in %
 DEBUG = False
 
 # You can enter a number to select a specific height.
 # Use a huge number here and closest match to get the highest resolution variant
 # Eg: 240, 360, 480, 720, 1080, 1440, 99999
-WANTED_RESOLUTION = 1080
+WANTED_RESOLUTION = 3840
 
 # Specify match type when specified height
 # Possible values: exact, exact_or_least_higher, exact_or_highest_lower, closest
