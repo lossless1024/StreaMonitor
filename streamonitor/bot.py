08bb80c30ea6f5dcf30b1235a769c108f45ba7b4
mK33y <56261048+mK33y@users.noreply.github.com>
2023-08-25 23:12:39 +0200

    Update bot.py

--- streamonitor/bot.py
+++ streamonitor/bot.py
@@ -29,7 +29,7 @@ class Bot(Thread):
     long_offline_timeout = 600
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0"
     }
 
     class Status(Enum):
