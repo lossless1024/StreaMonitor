907fa2aa56d57f8188b84a10c053c16477aae06e
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2023-09-11 10:54:03 +0200

    Fix StripChat VR

--- streamonitor/sites/stripchat_vr.py
+++ streamonitor/sites/stripchat_vr.py
@@ -1,31 +1,58 @@
-from streamonitor.sites.stripchat import StripChat
+import requests
 from streamonitor.bot import Bot
-from streamonitor.downloaders.fmp4s_wss import getVideoWSSVR
 
 
-class StripChatVR(StripChat):
+class StripChatVR(Bot):
     site = 'StripChatVR'
     siteslug = 'SCVR'
+    ModelID = ""
 
-    def __init__(self, username):
-        super().__init__(username)
-        self.getVideo = getVideoWSSVR
-        self.stopDownloadFlag = False
+    #Temporary approach to access the VR url.
+    def ConvertURlToVR(self, url):
+        if self.ModelID == "":
+            self.logger.warn(f'Unable to replace URL with VR URL: StreamID is empty')
+            return None
+        return url.replace(self.ModelID, self.ModelID + "_vr")
+
 
     def getVideoUrl(self):
-        return "wss://s-{server}.{host}/{id}_vr_webxr?".format(
-            server=self.lastInfo["broadcastSettings"]["vrBroadcastServer"],
-            host='stripcdn.com',
-            id=self.lastInfo["cam"]["streamName"]
-        ) + '&'.join([k + '=' + v for k, v in self.lastInfo['broadcastSettings']['vrCameraSettings'].items()])
+        return self.ConvertURlToVR(self.getWantedVRResolutionPlaylist(None))
 
     def getStatus(self):
-        status = super(StripChatVR, self).getStatus()
-        if status == Bot.Status.PUBLIC:
-            if self.lastInfo['model']['isVr'] and type(self.lastInfo['broadcastSettings']['vrCameraSettings']) is dict:
-                return Bot.Status.PUBLIC
+        r = requests.get('https://stripchat.com/api/vr/v2/models/username/' + self.username, headers=self.headers)
+        if r.status_code != 200:
+            return Bot.Status.UNKNOWN
+
+        self.lastInfo = r.json()
+
+        if self.ModelID == "":
+            self.ModelID = self.lastInfo["cam"]["streamName"]
+
+        if self.lastInfo['model']['isVr'] and type(self.lastInfo['broadcastSettings']['vrCameraSettings']) is dict:
+            return Bot.Status.PUBLIC
+        if not self.lastInfo['model']['isVr']:
             return Bot.Status.OFFLINE
-        return status
+        if self.lastInfo["model"]["status"] in ["private", "groupShow", "p2p", "virtualPrivate", "p2pVoice"]:
+            return Bot.Status.PRIVATE
+        if self.lastInfo["model"]["status"] in ["off", "idle"]:
+            return Bot.Status.OFFLINE
+        self.logger.warn(f'Got unknown status: {self.lastInfo["model"]["status"]}')
+        return Bot.Status.UNKNOWN
+    
+
+    def getPlaylistVariants(self, url):
+        def formatUrl(auto):
+            return "https://edge-hls.{host}/hls/{id}/master/{id}{auto}.m3u8".format(
+            server=self.lastInfo["cam"]["viewServers"]["flashphoner-hls"],
+            host='doppiocdn.com',
+            id=self.lastInfo["cam"]["streamName"],
+            auto='_auto' if auto else '')
+
+        variants = []
+        variants.extend(super().getPlaylistVariants(formatUrl(False)))
+        variants.extend(super().getPlaylistVariants(formatUrl(True)))
+        return variants    
+
 
 
 Bot.loaded_sites.add(StripChatVR)
