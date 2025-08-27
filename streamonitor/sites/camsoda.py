4a2f592050dd407eebcdba4ef45f8366558f2415
lossless1024 <lossless1024@gmail.com>
2025-08-27 12:46:12 +0200

    Camsoda report rate limited status

--- streamonitor/sites/camsoda.py
+++ streamonitor/sites/camsoda.py
@@ -24,6 +24,8 @@ class CamSoda(Bot):
 
     def getStatus(self):
         r = requests.get('https://www.camsoda.com/api/v1/chat/react/' + self.username, headers=self.headers)
+        if r.status_code == 403:
+            return Status.RATELIMIT
         if r.status_code != 200:
             return Status.UNKNOWN
 
@@ -37,7 +39,7 @@ class CamSoda(Bot):
         stream_data = self.lastInfo["stream"]
         if "edge_servers" in stream_data and len(stream_data["edge_servers"]) > 0:
             return Status.PUBLIC
-        elif "private_servers" in stream_data and len(stream_data["private_servers"]) > 0:
+        if "private_servers" in stream_data and len(stream_data["private_servers"]) > 0:
             return Status.PRIVATE
         if "token" in stream_data:
             return Status.OFFLINE
