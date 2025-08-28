7326b0a93fd87ea703b384b033d9c0d645abea7e
lossless1024 <lossless1024@gmail.com>
2025-08-28 19:29:16 +0200

    Workaround for stripchat playlist encryption

--- streamonitor/sites/stripchat.py
+++ streamonitor/sites/stripchat.py
@@ -1,5 +1,6 @@
 import requests
 from streamonitor.bot import Bot
+from streamonitor.downloaders.hls import getVideoNativeHLS
 from streamonitor.enums import Status
 
 
@@ -11,6 +12,9 @@ class StripChat(Bot):
         super().__init__(username)
         self.vr = False
         self.url = self.getWebsiteURL()
+        self.getVideo = lambda _, url, filename: getVideoNativeHLS(self, url, filename, StripChat.m3u_decoder)
+        self.psch = None
+        self.pkey = None
 
     def getWebsiteURL(self):
         return "https://stripchat.com/" + self.username
@@ -19,20 +23,23 @@ class StripChat(Bot):
         return self.getWantedResolutionPlaylist(None)
 
     def getPlaylistVariants(self, url):
-        def formatUrl(master, auto):
-            return "https://edge-hls.{host}/hls/{id}{vr}/{master}/{id}{vr}{auto}.m3u8".format(
-            host='doppiocdn.com',
-            id=self.lastInfo["cam"]["streamName"],
-            master='master' if master else '',
-            auto='_auto' if auto else '',
-            vr='_vr' if self.vr else '')
+        url = "https://edge-hls.{host}/hls/{id}{vr}/master/{id}{vr}_auto.m3u8".format(
+                host='doppiocdn.com',
+                id=self.lastInfo["cam"]["streamName"],
+                vr='_vr' if self.vr else '',
+            )
+        result = requests.get(url, headers=self.headers, cookies=self.cookies)
+        m3u8_doc = result.content.decode("utf-8")
 
-        variants = []
-        variants.extend(super().getPlaylistVariants(formatUrl(True, False)))
-        variants.extend(super().getPlaylistVariants(formatUrl(True, True)))
-        variants.extend(super().getPlaylistVariants(formatUrl(False, True)))
-        variants.extend(super().getPlaylistVariants(formatUrl(False, False)))
-        return variants
+        if '#EXT-X-MOUFLON' in m3u8_doc:
+            _mouflon_start = m3u8_doc.find('#EXT-X-MOUFLON:')
+            if _mouflon_start > 0:
+                _mouflon = m3u8_doc[_mouflon_start:m3u8_doc.find('\n', _mouflon_start)].strip().split(':')
+                self.psch = _mouflon[2]
+                self.pkey = _mouflon[3]
+
+        variants = super().getPlaylistVariants(m3u_data=m3u8_doc)
+        return [variant | {'url': f'{variant["url"]}?psch={self.psch}&pkey={self.pkey}'} for variant in variants]
 
     def getStatus(self):
         r = requests.get('https://stripchat.com/api/vr/v2/models/username/' + self.username, headers=self.headers)
@@ -49,6 +56,44 @@ class StripChat(Bot):
             return Status.OFFLINE
         self.logger.warn(f'Got unknown status: {self.lastInfo["model"]["status"]}')
         return Status.UNKNOWN
+
+    _cached_keys: dict = None
+
+    @classmethod
+    def m3u_decoder(cls, content):
+        import base64
+        import hashlib
+
+        def _decode(encrypted_b64: str, key: str) -> str:
+            if cls._cached_keys is None:
+                cls._cached_keys = {}
+            if key not in cls._cached_keys:
+                cls._cached_keys[key] = hashlib.sha256(key.encode("utf-8")).digest()
+            hash_bytes = cls._cached_keys[key]
+            hash_len = len(hash_bytes)
+
+            encrypted_data = base64.b64decode(encrypted_b64 + "==")
+
+            decrypted_bytes = bytearray()
+            for i, cipher_byte in enumerate(encrypted_data):
+                key_byte = hash_bytes[i % hash_len]
+                decrypted_byte = cipher_byte ^ key_byte
+                decrypted_bytes.append(decrypted_byte)
+
+            plaintext = decrypted_bytes.decode("utf-8")
+            return plaintext
+
+        decoded = []
+        lines = content.splitlines()
+        for idx, line in enumerate(lines):
+            if line.startswith("#EXT-X-MOUFLON:FILE:"):
+                dec = _decode(line[20:], "".join(
+                    [chr(a ^ b) for a, b in
+                     zip([48, 17, 22, 7, 15, 80, 16, 7, 8, 95, 6, 28, 43, 7, 67, 0], b"adsfadsfafdsafva")]))
+                decoded.append(lines[idx + 1].replace("media.mp4", dec))
+            else:
+                decoded.append(line)
+        return "\n".join(decoded)
 
 
 Bot.loaded_sites.add(StripChat)
