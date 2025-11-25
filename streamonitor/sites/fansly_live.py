16786f967edf5ae57e254a59b23e1ad879ed5635
elphabert <245968784+elphabert@users.noreply.github.com>
2025-11-25 19:39:38 +1100

    Actually add the fix

--- streamonitor/sites/fansly_live.py
+++ streamonitor/sites/fansly_live.py
@@ -1,33 +1,43 @@
+from parameters import HTTP_USER_AGENT
 from streamonitor.bot import RoomIdBot
 from streamonitor.enums import Status
 
 
 class FanslyLive(RoomIdBot):
-    site = 'FanslyLive'
-    siteslug = 'FL'
+    site = "FanslyLive"
+    siteslug = "FL"
+
+    headers = {
+        "User-Agent": HTTP_USER_AGENT,
+        "authorization": "ODQ4OTM3OTMzMTc1MDE3NDcyOjE6MjpjZWMwNWE3M2Y3YTNkMzM3NDI5NzEyYTU2OWJmYjY",
+    }
 
     def getWebsiteURL(self):
         return "https://fansly.com/live/" + self.username
 
     def getVideoUrl(self):
-        return self.getWantedResolutionPlaylist(self.lastInfo['stream']['playbackUrl'])
+        return self.getWantedResolutionPlaylist(self.lastInfo["stream"]["playbackUrl"])
 
     def getUsernameFromRoomId(self, room_id):
-        r = self.session.get(f'https://apiv3.fansly.com/api/v1/account?ids={room_id}&ngsw-bypass=true')
+        r = self.session.get(
+            f"https://apiv3.fansly.com/api/v1/account?ids={room_id}&ngsw-bypass=true"
+        )
         data = r.json()
-        for streamer in data.get('response', []):
-            if streamer.get('id') == room_id:
-                self.username = streamer['username']
-                return streamer.get('username')
+        for streamer in data.get("response", []):
+            if streamer.get("id") == room_id:
+                self.username = streamer["username"]
+                return streamer.get("username")
         return None
 
     def getRoomIdFromUsername(self, username):
-        r = self.session.get(f'https://apiv3.fansly.com/api/v1/account?usernames={username}&ngsw-bypass=true')
+        r = self.session.get(
+            f"https://apiv3.fansly.com/api/v1/account?usernames={username}&ngsw-bypass=true"
+        )
         data = r.json()
-        for streamer in data.get('response', []):
-            if streamer.get('username').lower() == username.lower():
-                self.username = streamer['username']
-                return streamer.get('id')
+        for streamer in data.get("response", []):
+            if streamer.get("username").lower() == username.lower():
+                self.username = streamer["username"]
+                return streamer.get("id")
         return None
 
     def getStatus(self):
@@ -36,22 +46,27 @@ class FanslyLive(RoomIdBot):
         if self.room_id is None:
             return Status.NOTEXIST
 
-        r = self.session.get(f'https://apiv3.fansly.com/api/v1/streaming/channel/{self.room_id}?ngsw-bypass=true')
+        r = self.session.get(
+            f"https://apiv3.fansly.com/api/v1/streaming/channel/{self.room_id}?ngsw-bypass=true"
+        )
         data = r.json()
-        if data.get('success') is not True:
+        if data.get("success") is not True:
             return Status.UNKNOWN
 
-        self.lastInfo = r.json().get('response')
+        self.lastInfo = r.json().get("response")
 
         if not self.lastInfo:
             return Status.NOTEXIST
-        if not self.lastInfo.get('stream'):
+        if not self.lastInfo.get("stream"):
             return Status.UNKNOWN
 
-        stream = self.lastInfo['stream']
-        if stream.get('status') == 2:
-            if stream.get('access') is True and 'playbackUrl' in stream:
+        stream = self.lastInfo["stream"]
+        if stream.get("status") == 2:
+            if stream.get("access") is True and "playbackUrl" in stream:
                 return Status.PUBLIC
             else:
                 return Status.PRIVATE
+
+        if stream.get("status") == 1:
+            return Status.OFFLINE
         return Status.UNKNOWN
