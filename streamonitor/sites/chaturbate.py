342835c9c677ad005c4f9e9c39fb4cedbde94efa
lossless1024 <lossless1024@gmail.com>
2026-01-08 16:44:48 +0100

    Bulk update CB

--- streamonitor/sites/chaturbate.py
+++ streamonitor/sites/chaturbate.py
@@ -1,12 +1,20 @@
 import re
 import requests
 from streamonitor.bot import Bot
-from streamonitor.enums import Status
+from streamonitor.enums import Status, Gender
 
 
 class Chaturbate(Bot):
     site = 'Chaturbate'
     siteslug = 'CB'
+    bulk_update = True
+
+    _GENDER_MAP = {
+        'f': Gender.FEMALE,
+        'm': Gender.MALE,
+        't': Gender.TRANS,
+        'c': Gender.BOTH,
+    }
 
     def __init__(self, username):
         super().__init__(username)
@@ -23,6 +31,15 @@ class Chaturbate(Bot):
             url = re.sub('live-.+amlst', 'live-c-fhls/amlst', url)
 
         return self.getWantedResolutionPlaylist(url)
+    
+    @staticmethod
+    def _parseStatus(status):
+        if status == "public":
+            return Status.PUBLIC
+        elif status in ["private", "hidden"]:
+            return Status.PRIVATE
+        else:
+            return Status.OFFLINE
 
     def getStatus(self):
         headers = {"X-Requested-With": "XMLHttpRequest"}
@@ -31,15 +48,40 @@ class Chaturbate(Bot):
         try:
             r = requests.post("https://chaturbate.com/get_edge_hls_url_ajax/", headers=headers, data=data)
             self.lastInfo = r.json()
-
-            if self.lastInfo["room_status"] == "public":
-                status = Status.PUBLIC
-            elif self.lastInfo["room_status"] in ["private", "hidden"]:
-                status = Status.PRIVATE
-            else:
-                status = Status.OFFLINE
+            status = self._parseStatus(self.lastInfo['room_status'])
         except:
             status = Status.RATELIMIT
 
         self.ratelimit = status == Status.RATELIMIT
         return status
+
+    @classmethod
+    def getStatusBulk(cls, streamers):
+        for streamer in streamers:
+            if not isinstance(streamer, Chaturbate):
+                continue
+
+        session = requests.Session()
+        session.headers.update(cls.headers)
+        r = session.get("https://chaturbate.com/affiliates/api/onlinerooms/?format=json&wm=DkfRj", timeout=10)
+
+        try:
+            data = r.json()
+        except requests.exceptions.JSONDecodeError:
+            print('Failed to parse JSON response')
+            return
+        data_map = {str(model['username']).lower(): model for model in data}
+
+        for streamer in streamers:
+            model_data = data_map.get(streamer.username.lower())
+            if not model_data:
+                streamer.setStatus(Status.OFFLINE)
+                continue
+            if model_data.get('gender'):
+                streamer.gender = cls._GENDER_MAP.get(model_data.get('gender'))
+            if model_data.get('country'):
+                streamer.country = model_data.get('country', '').upper()
+            status = cls._parseStatus(model_data['current_show'])
+            if status == Status.UNKNOWN:
+                print(f'[{streamer.siteslug}] {streamer.username}: Bulk update got unknown status: {status}')
+            streamer.setStatus(status)
